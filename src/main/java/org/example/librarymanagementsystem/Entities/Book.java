b3c8924dcb6d2ffbbddc34afeaa74d5de5e66ab5
Shade art <hamzabitmesra786@gmail.com>
2026-07-21 23:11:38 +0530

    setup the db and make book entity

---
+++ src/main/java/org/example/librarymanagementsystem/Entities/Book.java
@@ -0,0 +1,31 @@
+package org.example.librarymanagementsystem.Entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.example.librarymanagementsystem.enums.BookStatus;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    private LocalDate dateAdded;
+
+    @Enumerated(EnumType.STRING)
+    private BookStatus status;
+
+}
---
+++ src/main/java/org/example/librarymanagementsystem/enums/BookStatus.java
@@ -0,0 +1,6 @@
+package org.example.librarymanagementsystem.enums;
+
+public enum BookStatus {
+    AVAILABLE,
+    BORROWED
+}
